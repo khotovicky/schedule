b115607559ac03146c6efbba937798a63a7010b8
khotovicky <yoddddu@gmail.com>
2024-02-19 16:58:05 +0300

    прописал time строковый тип, добавил типу Day свойство date

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,11 @@
 export type Event = {
   title: string;
   description: string;
-  time: Date;
+  time: string;
 };
 
 export type Day = {
+  date: string;
   title: string | null;
   description: string | null;
   events: Event[] | null;
